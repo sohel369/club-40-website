df0b62efab0273d44e262e6d4386b5048b04352b
sohel369 <sohel0130844@gmail.com>
2026-07-14 01:47:53 +0600

    Remove export from Next config for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/club-40-website',
-  assetPrefix: '/club-40-website/',
   images: {
     unoptimized: true,
   },
